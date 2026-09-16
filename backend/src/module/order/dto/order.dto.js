2618434c9bf60e12ccafaf7057f8cf82ebb8ef47
SakinaEbrahimi <37002931+SakinaEbrahimi@users.noreply.github.com>
2026-09-16 06:35:28 -0700

    remove all codes

--- backend/src/module/order/dto/order.dto.js
+++
@@ -1,5 +0,0 @@
-import z from "zod";
-
-export const orderIdSchema = z.object({
-  id: z.string().regex(/^[0-9a-fA-F]{24}$/, "Invalid id."),
-});
--- backend/src/module/payment/dto/payment.dto.js
+++
@@ -1,9 +0,0 @@
-import z from "zod";
-
-export const paymentIdSchema = z.object({
-  id: z.string().regex(/^[0-9a-fA-F]{24}$/, "Invalid id."),
-});
-
-export const orderIdSchema = z.object({
-  orderId: z.string().regex(/^[0-9a-fA-F]{24}$/, "Invalid id."),
-});
